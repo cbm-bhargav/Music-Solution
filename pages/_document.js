6b8636180692bb9ed5c78e8967568e2f85064b30
Bhargav Chauhan <bhargavchauhan803@gmail.com>
2025-09-04 17:19:33 +0530

    change committed

--- pages/_document.js
+++ pages/_document.js
@@ -83,7 +83,6 @@
 //     );
 //   }
 // }
-
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
@@ -93,36 +92,48 @@ export default class CustomDocument extends Document {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () => originalRenderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
 
-    const initialProps = await Document.getInitialProps(ctx);
-    const styleTags = sheet.getStyleElement();
-
-    return { ...initialProps, styleTags };
+      const initialProps = await Document.getInitialProps(ctx);
+      return { ...initialProps, styles: [...initialProps.styles, sheet.getStyleElement()] };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     const language = this.props.__NEXT_DATA__?.query?.language;
     const locale = language === 'ch-de' ? 'de' : 'en';
+
     return (
       <Html lang={locale}>
         <Head>
-          {this.props.styleTags}
-          {/** Add Google fonts and Preconnect to Google Servers - Next JS will add Optimizations automatically  */}
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
-          <link rel='preconnect' href='https://www.googletagmanager.com' />
-          <link rel='preload' href='/assets/fonts/ms_instruments.woff' as='font' type='font/woff' crossOrigin='anonymous' />
-          <link rel='preload' href='/assets/fonts/Roboto/Roboto-Regular.ttf' as='font' type='font/ttf' crossOrigin='anonymous' />
+          {/* Preconnect for faster font fetching */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
 
+          {/* Preload only modern formats (woff2) */}
           <link
-            href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap'
-            rel='stylesheet'
+            rel="preload"
+            href="/assets/fonts/ms_instruments.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
           />
+
+          {/* Google Fonts with display=swap to avoid CLS */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+
           <style
             dangerouslySetInnerHTML={{
               __html: `
-                /* fallback */
+                /* Material Icons (use swap to avoid layout shift) */
                 @font-face {
                   font-family: 'Material Icons Outlined';
                   font-style: normal;
@@ -130,18 +141,21 @@ export default class CustomDocument extends Document {
                   font-display: swap;
                   src: url(https://fonts.gstatic.com/s/materialiconsoutlined/v95/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcel5euIg.woff2) format('woff2');
                 }
+
+                /* Custom font optimized */
                 @font-face {
                   font-family: 'ms_instruments';
                   font-style: normal;
                   font-weight: normal;
                   font-display: swap;
-                  src: url('/assets/fonts/ms_instruments.woff') format('woff'),
-                       url('/assets/fonts/ms_instruments.ttf') format('truetype');
-                  size-adjust: 100%;
+                  src: url('/assets/fonts/ms_instruments.woff2') format('woff2'),
+                       url('/assets/fonts/ms_instruments.woff') format('woff');
                   ascent-override: 90%;
                   descent-override: 22%;
                   line-gap-override: 0%;
                 }
+
+                /* Fallback to Arial while custom font loads */
                 @font-face {
                   font-family: 'Roboto-fallback';
                   font-style: normal;
@@ -152,6 +166,7 @@ export default class CustomDocument extends Document {
                   line-gap-override: 0%;
                   size-adjust: 107.4%;
                 }
+
                 .material-icons-outlined {
                   font-family: 'Material Icons Outlined';
                   font-weight: normal;
@@ -171,12 +186,12 @@ export default class CustomDocument extends Document {
           />
         </Head>
         <body>
-          {/* Google Tag Manager (noscript)  */}
+          {/* Google Tag Manager (noscript) */}
           <noscript
             dangerouslySetInnerHTML={{
               __html: `
                 <iframe 
-                  title='Google Tag Manager'
+                  title="Google Tag Manager"
                   src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG_MANAGER_ID}" 
                   height="0" 
                   width="0" 
@@ -184,7 +199,6 @@ export default class CustomDocument extends Document {
               `,
             }}
           />
-          {/* End Google Tag Manager (noscript)  */}
           <Main />
           <NextScript />
         </body>
